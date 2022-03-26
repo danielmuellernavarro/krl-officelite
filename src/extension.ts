81bf9446f5a30a8f2b2ac7cb3b31c05b1b9c0058
Daniel Müller Navarro <danielmuellernavarro@gmail.com>
2022-03-26 19:38:43 +0100

    fixed decrease case keyword

--- src/extension.ts
+++ src/extension.ts
@@ -77,7 +77,7 @@ class KrlFormatter {
 export function activate(context: vscode.ExtensionContext) {
     vscode.languages.setLanguageConfiguration("krl", {
         indentationRules: {
-            decreaseIndentPattern: new RegExp(/^\s*(ENDFOR|ELSE|ENDIF|ENDLOOP|UNTIL.*|ENDWHILE|ENDSWITCH|CASE.*|DEFAULT.*)\s*(;.*)?$/, "i"),
+            decreaseIndentPattern: new RegExp(/^\s*(ENDFOR|ELSE|ENDIF|ENDLOOP|UNTIL.*|ENDWHILE|ENDSWITCH|DEFAULT.*)\s*(;.*)?$/, "i"),
             increaseIndentPattern: new RegExp(/^\s*(FOR.*|IF.*|ELSE|LOOP|REPEAT|WHILE.*|SWITCH.*|CASE.*|DEFAULT.*)\s*(;.*)?$/, "i"),
         },
     });
